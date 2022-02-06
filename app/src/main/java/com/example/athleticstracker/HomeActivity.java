19fc33b05ee54673e14930e571045c46933d3cda
StuDeiv <90849919+StuDeiv@users.noreply.github.com>
2022-02-06 22:39:04 +0100

    feat: add auth log-in & new user homepage

---
+++ app/src/main/java/com/example/athleticstracker/HomeActivity.java
@@ -0,0 +1,35 @@
+package com.example.athleticstracker;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/athleticstracker/Registro1Activity.java
@@ -0,0 +1,26 @@
+package com.example.athleticstracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Registro1Activity extends AppCompatActivity {
+
+    private TextView textViewUsuario;
+    private Bundle bundle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro1);
+        obtenerDatos();
+    }
+
+    private void obtenerDatos(){
+        this.textViewUsuario = (TextView) findViewById(R.id.textViewUsuario);
+        bundle = getIntent().getExtras();
+        textViewUsuario.setText(bundle.getString("mailUsuario"));
+    }
+
+}
