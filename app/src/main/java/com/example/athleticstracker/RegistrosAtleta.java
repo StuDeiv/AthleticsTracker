c0ffe1e36a9faa2ff45a6c079aa3f1bee3e4d5fc
vjp-JorgeLuisFD <jlfernandezd04@informatica.iesvalledeljerteplasencia.es>
2022-02-23 19:28:25 +0100

    Falta completar la parte de mostrar los registros de los atletas.

--- app/src/main/java/com/example/athleticstracker/RegistrosAtleta.java
+++ app/src/main/java/com/example/athleticstracker/RegistrosAtleta.java
@@ -3,12 +3,23 @@ package com.example.athleticstracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class RegistrosAtleta extends AppCompatActivity {
 
+    private Usuario usuario;
+    private ArrayList<Registro> lRegistros;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_registros_atleta);
+
+        cargarDatos();
+    }
+
+    private void cargarDatos(){
+        this.lRegistros = (ArrayList<Registro>) getIntent().getExtras().getSerializable("registros");
     }
 }
