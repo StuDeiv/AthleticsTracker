aac2806a5bae6ba8822962b06679e19755d2d77d
StuDeiv <90849919+StuDeiv@users.noreply.github.com>
2022-03-01 09:31:10 +0100

    Added activities to configure user profile
    Need to be fixed: AlertDialog is not working because of incompatibility with theme app and Theme.AppCompat

--- app/src/main/java/com/example/athleticstracker/AjustesUsuarioActivity.java
+++ app/src/main/java/com/example/athleticstracker/AjustesUsuarioActivity.java
@@ -5,13 +5,11 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.InputType;
+import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,15 +17,16 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.rpc.context.AttributeContext;
 
 public class AjustesUsuarioActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseUser sesionUsuario;
     private TextView txtViewMailUsuarioAjustes;
-    private Button btnActualizarMail;
+    private Button btnActualizarDatosUsuario;
     private Button btnEnviarMensajeVerificacion;
-    private Button btnReestablecerContraseña;
+    private Button btnReestablecerContrasenia;
+    private Button btnCerrarSesión;
     private Bundle bundle;
 
     @Override
@@ -35,31 +34,127 @@ public class AjustesUsuarioActivity extends AppCompatActivity implements View.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_ajustes_usuario);
         iniciarDatos();
+        inicializarListeners();
+    }
 
+    private void inicializarListeners() {
+        btnActualizarDatosUsuario.setOnClickListener(this);
+        btnReestablecerContrasenia.setOnClickListener(this);
+        btnEnviarMensajeVerificacion.setOnClickListener(this);
+        btnCerrarSesión.setOnClickListener(this);
     }
 
     private void iniciarDatos() {
         sesionUsuario = FirebaseAuth.getInstance().getCurrentUser();
         txtViewMailUsuarioAjustes = (TextView) findViewById(R.id.txtViewMailUsuarioAjustes);
         txtViewMailUsuarioAjustes.setText(sesionUsuario.getEmail().toString());
-        btnActualizarMail = (Button) findViewById(R.id.btnActualizarMail);
-        btnReestablecerContraseña = (Button) findViewById(R.id.btnReestablecerContraseña);
+        btnActualizarDatosUsuario = (Button) findViewById(R.id.btnActualizarDatosUsuario);
+        btnReestablecerContrasenia = (Button) findViewById(R.id.btnReestablecerContrasenia);
         btnEnviarMensajeVerificacion = (Button) findViewById(R.id.btnEnviarMensajeVerificacion);
-        btnActualizarMail.setOnClickListener(this);
+        btnCerrarSesión = (Button) findViewById(R.id.btnCerrarSesión);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.btnActualizarMail:
+            case R.id.btnActualizarDatosUsuario:
+                Intent intent = new Intent(getApplicationContext(), CambioDatosUsuarioActivity.class);
+                startActivity(intent);
                 break;
             case R.id.btnEnviarMensajeVerificacion:
+                alertDialogEnviarMensajeVerificacion();
                 break;
-            case R.id.btnReestablecerContraseña:
+            case R.id.btnReestablecerContrasenia:
+                alertDialogReestablecerContrasenia();
+                break;
+            case R.id.btnCerrarSesión:
+                alertDialogCerrarSesion();
                 break;
         }
     }
 
+    private void alertDialogCerrarSesion() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+        builder.setTitle("Cerrar sesión");
+        builder.setMessage("¿Estás seguro de que quieres cerrar sesión?");
+        builder.setPositiveButton("CERRAR SESIÓN", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //TODO: REVISAR ON DESTROY()
+                FirebaseAuth.getInstance().signOut();
+                //onDestroy();
+                Intent intent = new Intent(getApplicationContext(), AuthActivity.class);
+                startActivity(intent);
+            }
+        });
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        builder.show();
+    }
+
+    private void alertDialogEnviarMensajeVerificacion() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+        builder.setTitle("Envíado mensaje de verificación");
+        builder.setMessage("Se ha enviado un mensaje de verificación a la siguiente dirección de correo: " + sesionUsuario.getEmail());
+        builder.setPositiveButton("Enviar mail", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                sesionUsuario.sendEmailVerification()
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getApplicationContext(), "Mail enviado.Revisa tu bandeja de entrada", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+        builder.setNegativeButton("CERRAR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void alertDialogReestablecerContrasenia() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+        builder.setTitle("Reestablecer contraseña");
+        builder.setMessage("¿Estás seguro de que deseas reestablecer tu contraseña? Pulsa OK para confirmarlo");
+
+        //Opción que acepta reestablecer contraseña
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                FirebaseAuth.getInstance().sendPasswordResetEmail(sesionUsuario.getEmail())
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getApplicationContext(), "Mail enviado.Revisa tu bandeja de entrada", Toast.LENGTH_SHORT).show();
+                                    dialogInterface.cancel();
+                                }
+                            }
+                        });
+            }
+        });
+
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+
+        builder.show();
+    }
 
 
 }
---
+++ app/src/main/java/com/example/athleticstracker/CambioDatosUsuarioActivity.java
@@ -0,0 +1,88 @@
+package com.example.athleticstracker;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class CambioDatosUsuarioActivity extends AppCompatActivity {
+
+    private EditText editTextNuevoMailUsuario;
+    private EditText editTextNuevaContrasenia;
+    private Button btnCambiarDatosUsuario;
+    private Button btnCancelar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cambio_datos_usuario);
+        iniciarDatos();
+
+
+        //Cambiar mail usuario y contraseña
+        btnCambiarDatosUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!StringUtils.isBlank(editTextNuevoMailUsuario.getText().toString()) && !StringUtils.isBlank(editTextNuevaContrasenia.getText().toString())){
+                    //Mail
+                    FirebaseAuth.getInstance().getCurrentUser().updateEmail(editTextNuevoMailUsuario.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                    }
+                                }
+                            });
+                    //Contraseña
+                    FirebaseAuth.getInstance().getCurrentUser().updatePassword(editTextNuevaContrasenia.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+
+                                    }
+                                }
+                            });
+                    //TODO: Revisar si merece la pena realizar aquí finish()
+                    finish();
+                }else{
+                    if (editTextNuevaContrasenia.getText().toString().length() < 6){
+                        Toast.makeText(getApplicationContext(),"La contraseña debe tener al menos 6 caracteres",Toast.LENGTH_SHORT).show();
+                    }
+                    if (StringUtils.isBlank(editTextNuevoMailUsuario.getText().toString())){
+                        Toast.makeText(getApplicationContext(),"Introduce un mail, por favor",Toast.LENGTH_SHORT).show();
+                    }
+                    if (StringUtils.isBlank(editTextNuevaContrasenia.getText().toString())){
+                        Toast.makeText(getApplicationContext(),"Introduce una contraseña, por favor",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        btnCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
+    }
+
+    private void iniciarDatos() {
+        editTextNuevoMailUsuario = (EditText) findViewById(R.id.editTextNuevoMailUsuario);
+        editTextNuevaContrasenia = (EditText) findViewById(R.id.editTextNuevaContrasenia);
+        btnCambiarDatosUsuario = (Button) findViewById(R.id.btnCambiarDatosUsuario);
+        btnCancelar = (Button) findViewById(R.id.btnCancelar);
+    }
+}
